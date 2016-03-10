59ed8293a23b0c05b8b0a50de9c0528b94b7e578
Lisa Goldberg <lisa.yogajr@aol.com>
2016-03-09 18:57:04 -0800

    added loops

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//window.alert("it works!")
+var friends = ["Singh", "Erik", "Jenny", "Sean", "Evan"];
+for (var i = friends.length -1; i>=0; i--) {
+  console.log(friends[i]);
+}
+
+var friends = ["Christina", "Ilya", "Matt", "Allie", "Ian"];
+var index = 0;
+while(friends.length > index) {
+  console.log(friends[index]);
+  index++;
+}
